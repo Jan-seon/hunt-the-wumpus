a89d28a518dd39699805ffe40a73d180dc981241
RedPandaGaminq <timothyzhong2@gmail.com>
2022-05-19 14:48:48 -0700

    created ui framework

---
+++ HuntTheWumpus/HomeScreen.Designer.cs
@@ -0,0 +1,131 @@
+﻿
+namespace HuntTheWumpus
+{
+    partial class HomeScreen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCredits = new System.Windows.Forms.Button();
+            this.buttonHighScores = new System.Windows.Forms.Button();
+            this.buttonPlay = new System.Windows.Forms.Button();
+            this.labelHeader = new System.Windows.Forms.Label();
+            this.labelTitle = new System.Windows.Forms.Label();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonCredits
+            // 
+            this.buttonCredits.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCredits.Location = new System.Drawing.Point(91, 500);
+            this.buttonCredits.Name = "buttonCredits";
+            this.buttonCredits.Size = new System.Drawing.Size(800, 75);
+            this.buttonCredits.TabIndex = 9;
+            this.buttonCredits.Text = "Credits";
+            this.buttonCredits.UseVisualStyleBackColor = true;
+            this.buttonCredits.Click += new System.EventHandler(this.buttonCredits_Click);
+            // 
+            // buttonHighScores
+            // 
+            this.buttonHighScores.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonHighScores.Location = new System.Drawing.Point(91, 400);
+            this.buttonHighScores.Name = "buttonHighScores";
+            this.buttonHighScores.Size = new System.Drawing.Size(800, 75);
+            this.buttonHighScores.TabIndex = 8;
+            this.buttonHighScores.Text = "High Scores";
+            this.buttonHighScores.UseVisualStyleBackColor = true;
+            this.buttonHighScores.Click += new System.EventHandler(this.buttonHighScores_Click);
+            // 
+            // buttonPlay
+            // 
+            this.buttonPlay.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPlay.Location = new System.Drawing.Point(91, 300);
+            this.buttonPlay.Name = "buttonPlay";
+            this.buttonPlay.Size = new System.Drawing.Size(800, 75);
+            this.buttonPlay.TabIndex = 7;
+            this.buttonPlay.Text = "Play";
+            this.buttonPlay.UseVisualStyleBackColor = true;
+            this.buttonPlay.Click += new System.EventHandler(this.buttonPlay_Click);
+            // 
+            // labelHeader
+            // 
+            this.labelHeader.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelHeader.Location = new System.Drawing.Point(1, 175);
+            this.labelHeader.Name = "labelHeader";
+            this.labelHeader.Size = new System.Drawing.Size(980, 100);
+            this.labelHeader.TabIndex = 6;
+            this.labelHeader.Text = "Johnny Depp vs Amber Heard";
+            this.labelHeader.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelTitle
+            // 
+            this.labelTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 55.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTitle.Location = new System.Drawing.Point(1, 25);
+            this.labelTitle.Name = "labelTitle";
+            this.labelTitle.Size = new System.Drawing.Size(980, 200);
+            this.labelTitle.TabIndex = 5;
+            this.labelTitle.Text = "Hunt the Wumpus";
+            this.labelTitle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExit.Location = new System.Drawing.Point(91, 600);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(800, 75);
+            this.buttonExit.TabIndex = 10;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // HomeScreen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(982, 703);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonCredits);
+            this.Controls.Add(this.buttonHighScores);
+            this.Controls.Add(this.buttonPlay);
+            this.Controls.Add(this.labelHeader);
+            this.Controls.Add(this.labelTitle);
+            this.Name = "HomeScreen";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hunt the Wumpus";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCredits;
+        private System.Windows.Forms.Button buttonHighScores;
+        private System.Windows.Forms.Button buttonPlay;
+        private System.Windows.Forms.Label labelHeader;
+        private System.Windows.Forms.Label labelTitle;
+        private System.Windows.Forms.Button buttonExit;
+    }
+}
