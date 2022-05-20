add7b29a5ddc3d2b9b331099b7b4ae15a392a88f
ericfrintu <105889958+ericfrintu@users.noreply.github.com>
2022-05-20 14:33:42 -0700

    Added highscore functions
    created methods

--- HuntTheWumpus/HighScore/HighScoreManager.cs
+++ HuntTheWumpus/HighScore/HighScoreManager.cs
@@ -8,5 +8,35 @@ namespace HuntTheWumpus.HighScore
 {
     class HighScoreManager
     {
+
+        public List<HighScore> HighScores { get; set; }
+
+        public void Add(HighScore highscore)
+        {
+
+        }
+        public void Sort()
+        {
+
+        }
+        public List<HighScore> GetHighScores()
+        {
+            return HighScores;
+        }
+        public void GetFromFile(string path)
+        {
+            //File.IO
+
+            //List<HighScore> highScoresFromFile = new List<HighScore>
+            //Get highscoress from path
+            //HighScores = highScoresFromFile
+
+        }
+        public void SaveToFile(string path)
+        {
+            //Save the highscores to a file
+            //File.IO
+        }
+
     }
 }
