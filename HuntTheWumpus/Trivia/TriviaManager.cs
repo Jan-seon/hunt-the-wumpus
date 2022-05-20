07b70a3dd69d3b20cf9f7b1b3d1dae294798ae16
columbigas <1103466@apps.nsd.org>
2022-05-20 15:12:51 -0700

    trivia object changes

--- HuntTheWumpus/Trivia/TriviaManager.cs
+++ HuntTheWumpus/Trivia/TriviaManager.cs
@@ -3,10 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace HuntTheWumpus.Trivia
 {
     class TriviaManager
     {
+        // not sure if i did this right tbh
+        public List<Question> questions = new List<Question>();
+
+        public TriviaManager()
+        {
+
+        }
+
+        public void ReadFile()
+        {
+            // hopefully im doing this correctly
+            using (StreamReader sr = new StreamReader("huntTheWumpusQuestions.txt"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    // Random rnd = new Random();
+                    string question = sr.ReadLine();
+                }
+            }
+        }
+
+        public void GetRandomQuestion(int num)
+        {
+
+        }
     }
 }
