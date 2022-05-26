486e8cd90ed5532982fe39b97dacf4d533381373
Jakelodocus <2003803@apps.nsd.org>
2022-05-26 15:04:43 -0700

    Added player obj

---
+++ 2003803_playerObject_Murray/Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2003803_playerObject_Murray
+{
+    class Player
+    {
+        public int Arrows { get; set; }
+        public int Coins { get; set; }
+        public int Turns { get; set; }
+        public int WumpusBundle { get; set; }
+
+        public Player()
+        {
+            Arrows = 3;
+            Coins = 0;
+            Turns = 0;
+        }
+
+        public int CalculateScore()
+        {
+           return 100 - Turns + Coins + (5 * Arrows); //+ wumpus points
+        }
+
+        public void ShootArrows()
+        {
+            Arrows = Arrows--;
+        }
+
+        public void BuyArrows()
+        {
+            Arrows = Arrows + 2;
+            Coins = Coins--;
+        }
+
+        public void BuySecrets()
+        {
+           Coins = Coins--;
+        }
+
+        public void Move()
+        {
+            Turns++;
+        }
+    }
+}
