d1841d55d264249cb39d07ddcdc5ca964ca5c8cc
columbigas <1103466@apps.nsd.org>
2022-05-27 14:46:16 -0700

    trivia cmamfrneignrtu567
    retetree54y

---
+++ TriviaUnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TriviaUnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TriviaUnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("be2ca8cf-1d28-40d2-90a0-dd9871686768")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TriviaUnitTest/TriviaManagerTest.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace TriviaUnitTest
+{
+    [TestClass]
+    public class TriviaManagerTest
+    {
+        // Question _question = new Question("idk", "dssfe", "ewrfew", "wdwwd", "wdewfweef", "dssfe");
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
