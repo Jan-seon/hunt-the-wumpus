6083bc3ae06cda26d90cfbeb2e2d3c194ab6bb0e
Jakelodocus <2003803@apps.nsd.org>
2022-05-27 15:08:13 -0700

    Update Player.cs

--- HuntTheWumpus/Player/Player.cs
+++ HuntTheWumpus/Player/Player.cs
@@ -8,5 +8,43 @@ namespace HuntTheWumpus.Player
 {
     class Player
     {
+        public int Arrows { get; set; }
+        public int Coins { get; set; }
+        public int Turns { get; set; }
+        public bool WumpusBundle { get; set; }
+        
+
+        public Player()
+        {
+            Arrows = 3;
+            Coins = 0;
+            Turns = 0;
+        }
+
+        public int CalculateScore(bool WumpusBundle)
+        {
+            return 100 - Turns + Coins + (5 * Arrows) + (WumpusBundle ? 50 : 0); 
+        }
+
+        public void ShotArrow()
+        {
+            Arrows = Arrows--;
+        }
+
+        public void PurchaseArrows()
+        {
+            Arrows = Arrows + 2;
+            Coins = Coins--;
+        }
+
+        public void PurchaseSecret()
+        {
+            Coins = Coins--;
+        }
+
+        public void Move()
+        {
+            Turns++;
+        }
     }
 }
