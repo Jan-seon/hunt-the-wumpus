845d82c79343c47eec55982944a1a81aaa880c2a
columbigas <1103466@apps.nsd.org>
2022-05-31 15:10:21 -0700

    trivia manager test

--- TriviaUnitTest/TriviaManagerTest.cs
+++ TriviaUnitTest/TriviaManagerTest.cs
@@ -1,16 +1,20 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
+using System.IO;
+using HuntTheWumpus.Trivia;
 
 namespace TriviaUnitTest
 {
     [TestClass]
     public class TriviaManagerTest
     {
-        // Question _question = new Question("idk", "dssfe", "ewrfew", "wdwwd", "wdewfweef", "dssfe");
         [TestMethod]
-        public void TestMethod1()
+        public void QuestionsCorrect()
         {
+            TriviaManager triviaManager = new TriviaManager();
 
+            triviaManager.ReadFile();
+            Assert.AreEqual(triviaManager.GetRandomQuestion(3).Count, 3);
         }
     }
 }
