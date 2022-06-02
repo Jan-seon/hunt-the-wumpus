991a24b0154c6ab7d9be30f9dbbcb4b3a6b5e32a
Riley Clement <63689448+SickoPenguin@users.noreply.github.com>
2022-06-02 14:29:56 -0700

    Added GetGateWays() Function
    I made this specifically so the UI code easily grab the rooms it needs to display and show where it can't go.

--- HuntTheWumpus/Cave/Room.cs
+++ HuntTheWumpus/Cave/Room.cs
@@ -30,6 +30,27 @@ namespace HuntTheWumpus.Cave
             return accessibility;
         }
 
+        //This function is for the UI to easily get the rooms it needs to put on the button.
+        //When a room is null, that means thats a room you can't go to.
+        public Room[] GetGateWays()
+        {
+            Room[] tunnels = new Room[6];
+            for (int i = 0; i < Neighbors.Length; i++)
+            {
+                foreach (Room tunnel in GateWays)
+                {
+                    if (Neighbors[i].RoomNumber == tunnel.RoomNumber)
+                    {
+                        tunnels[i] = Neighbors[i];
+                    }
+                }
+            }
+
+            Room[] output = { tunnels[1], tunnels[2], tunnels[4], tunnels[5], tunnels[3], tunnels[0] };
+
+            return output;
+        }
+
         public Room(int num)
         {
             RoomNumber = num;
