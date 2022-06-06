bc10f327fb971ed19b22ce465e3acd8906dd27b4
RedPandaGaminq <timothyzhong2@gmail.com>
2022-06-06 15:06:56 -0700

    added a game over screen

---
+++ HuntTheWumpus/GameOverUI.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HuntTheWumpus
+{
+    public partial class GameOverUI : Form
+    {
+        public bool IsVictorious { get; set; }
+        public int Score { get; set; }
+
+        public GameOverUI(bool isVict, int score)
+        {
+            InitializeComponent();
+
+            IsVictorious = isVict;
+            Score = score;
+
+            labelOutcome.Text = $"You {(isVict ? "win" : "lose")}!";
+            labelScore.Text = $"Score: {score}";
+        }
+
+        private void buttonOk_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+
+            var homeScreen = new HomeScreen();
+            homeScreen.FormClosed += (s, args) => this.Close();
+            homeScreen.Show();
+        }
+    }
+}
