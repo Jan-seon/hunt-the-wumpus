42af31b307a6db45adc48bc516c81df04f6a640e
columbigas <1103466@apps.nsd.org>
2022-06-09 14:36:03 -0700

    Update TriviaManagerTest.cs

--- TriviaUnitTest/TriviaManagerTest.cs
+++ TriviaUnitTest/TriviaManagerTest.cs
@@ -7,6 +7,7 @@ namespace TriviaUnitTest
     [TestClass]
     public class TriviaManagerTest
     {
+        // see if the trivia manager has gotten the right amount of questions requested
         [TestMethod]
         public void QuestionQuantityCorrect()
         {
@@ -16,6 +17,7 @@ namespace TriviaUnitTest
             Assert.AreEqual(triviaManager.GetRandomQuestion(3).Count, 3);
         }
 
+        // see if the trivia manager has gotten the wrong amount of questions requested
         [TestMethod]
         public void QuestionQuantityIncorrect()
         {
@@ -25,6 +27,7 @@ namespace TriviaUnitTest
             Assert.AreNotEqual(triviaManager.GetRandomQuestion(5).Count, 3);
         }
 
+        // see if the trivia manager can tell when you get the question correct
         [TestMethod]
         public void CorrectGuess()
         {
@@ -34,6 +37,7 @@ namespace TriviaUnitTest
             Assert.IsTrue(question.CheckAnswer(guess));
         }
 
+        // see if the trivia manager can tell when you get the question incorrect
         [TestMethod]
         public void IncorrectGuess()
         {
